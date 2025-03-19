d7335914b6cf0e8679725b7b3655a345edf644ef
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-03-19 14:20:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,20 @@ const getBooks = function () {
         throw new Error("Il server non risponde correttamente");
       }
     })
-    .then((data) => console.log("DATAd", data))
+    .then((data) => {
+      console.log("DATAd", data);
+      const bookTitle = data.title;
+      const bookCover = data.img;
+      const bookPrice = data.price;
+
+      const title = document.getElementsByClassName("card-title");
+      const cover = document.getElementsByClassName("card-img-top");
+      const price = document.getElementsByClassName("card-text");
+
+      title.innerText = bookTitle;
+      cover.innerText = bookCover;
+      price.innerText = bookPrice;
+    })
     .catch((error) => console.error("Errore:", error));
 };
 
